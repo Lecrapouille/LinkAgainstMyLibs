b706355c965ae02acf7abaffb06a1a4bbd8f3444
Quentin Quadrat <lecrapouille@gmail.com>
2023-01-09 21:09:45 +0100

    Update Logger test

--- Logger/src/main.cpp
+++ Logger/src/main.cpp
@@ -4,7 +4,19 @@
 
 int main()
 {
-    CONFIG_LOG(project::info);
+    CONFIG_LOG(mylogger::project::Info(
+        project::info::mode == project::info::debug,
+        project::info::application_name.c_str(),
+        project::info::major_version,
+        project::info::minor_version,
+        project::info::git_branch.c_str(),
+        project::info::git_sha1.c_str(),
+        project::info::data_path.c_str(),
+        project::info::tmp_path.c_str(),
+        project::info::log_name.c_str(),
+        project::info::log_path.c_str()
+    ));
+
     LOGI("An information %s", "the info");
     LOGD("A debug %s", "the debug");
     LOGW("A warning %s", "the warning");
