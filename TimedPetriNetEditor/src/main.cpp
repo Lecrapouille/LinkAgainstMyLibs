9e28eb182c009d3f27056a9d1994d109bab7147f
Quentin QUADRAT <lecrapouille@gmail.com>
2026-05-31 22:01:09 +0200

    Update

--- TimedPetriNetEditor/src/main.cpp
+++ TimedPetriNetEditor/src/main.cpp
@@ -1,29 +1,30 @@
-#include <TimedPetriNetEditor/PetriNet.hpp>
-//#include <Editor/DearImGui/Editor.hpp> // FIXME poor header: shall be <TimedPetriNetEditor/Editor/DearImGui/Editor.hpp>
-// and calling "Application.hpp" which forec use to use -I
-#include <stdio.h>
+#include <PetriNet/PetriNet.hpp>
+
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
 
 //------------------------------------------------------------------------------
 int main()
 {
     tpne::Net net(tpne::TypeOfNet::TimedPetriNet);
-    assert(net.places() == 0);
-    assert(net.transitions() == 0);
+    assert(net.places().empty());
+    assert(net.transitions().empty());
 
     tpne::Place& p0 = net.addPlace(3.5f, 4.0f, 1u);
     tpne::Transition& t0 = net.addTransition(3.14f, 2.16f);
     net.addArc(p0, t0);
 
-    assert(net.places() == 1);
-    assert(net.transitions() == 1);
-    assert(net.arcs() == 1);
+    assert(net.places().size() == 1u);
+    assert(net.transitions().size() == 1u);
+    assert(net.arcs().size() == 1u);
 
     std::cout << net.places()[0].key << std::endl;
     std::cout << net.transitions()[0].key << std::endl;
 
-    // FIXME: ahrd to compile
-    //tpne::Editor editor;
-    //editor.run("");
+    // GUI (TimedPetriGUI) is provided by pkg-config TimedPetriGUI, not TimedPetriNet.
+    // tpne::Editor editor;
+    // editor.run("");
 
     return EXIT_SUCCESS;
 }
